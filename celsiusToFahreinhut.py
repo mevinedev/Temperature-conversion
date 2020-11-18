18799429ec50205b491b1a21f7613849777f216d
Mevine  Akinyi <72660182+mevinedev@users.noreply.github.com>
2020-11-18 16:02:39 +0300

    Update celsiusToFahreinhut.py

--- celsiusToFahreinhut.py
+++ celsiusToFahreinhut.py
@@ -1,8 +1,11 @@
 celsius = int(input(" enter value to convert: "))
+
 def convert(c):
     fahreinheit = ((9/5) * (celsius +32))
     return fahreinheit
 
 fahreinheit = convert(celsius)
+
 print("the value in fahreinheit is: ",fahreinheit)
+
 
